2c454d6a0eef62f4267ea36ed614465a4dfe1309
Simon Emms <simon@simonemms.com>
2026-03-09 18:27:26 +0000

    chore: initial commit
    Signed-off-by: Simon Emms <simon@simonemms.com>

---
+++ README.md
@@ -0,0 +1,37 @@
+# ui
+
+A drag and drop UI for building Temporal workflows
+
+<!-- toc -->
+
+* [Contributing](#contributing)
+  * [Open in Gitpod](#open-in-gitpod)
+  * [Open in a container](#open-in-a-container)
+  * [Commit style](#commit-style)
+
+<!-- Regenerate with "pre-commit run -a markdown-toc" -->
+
+<!-- tocstop -->
+
+## Contributing
+
+### Open in Gitpod
+
+* [Open in Gitpod](https://gitpod.io/from-referrer/)
+
+### Open in a container
+
+* [Open in a container](https://code.visualstudio.com/docs/devcontainers/containers)
+
+### Commit style
+
+All commits must be done in the [Conventional Commit](https://www.conventionalcommits.org)
+format.
+
+```git
+<type>[optional scope]: <description>
+
+[optional body]
+
+[optional footer(s)]
+```
---
+++ eslint.config.js
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2026 Zigflow authors <https://github.com/zigflow/ui/graphs/contributors>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import js from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+import svelte from 'eslint-plugin-svelte';
+import globals from 'globals';
+import ts from 'typescript-eslint';
+
+export default ts.config(
+	js.configs.recommended,
+	...ts.configs.recommended,
+	...svelte.configs['flat/recommended'],
+	prettier,
+	...svelte.configs['flat/prettier'],
+	{
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				...globals.node,
+			},
+		},
+	},
+	{
+		files: ['**/*.svelte'],
+
+		languageOptions: {
+			parserOptions: {
+				parser: ts.parser,
+			},
+		},
+	},
+	{
+		ignores: ['build/', '.svelte-kit/', 'dist/'],
+	},
+);
